4bfe0edd3770980120fd1b613c5f81cb4a074670
Chau Tien Phong <phongctgcs190127@fpt.edu.vn>
2023-04-20 23:57:56 +0700

    Add filter by price

--- src/public/js/shopFilter.js
+++ src/public/js/shopFilter.js
@@ -1,63 +1,87 @@
-$(document).ready(function() {
-    let shopFilterEle = $('.shop-filter-container>div');
-  
-    if (shopFilterEle) {
-    //   let shopFilterIsotope = new Isotope(shopFilterEle[0], {
-    //     itemSelector: '.shop-filter-item',
-    //     layoutMode: 'masonry'
-    //   });
-  
-      let shopFilters = $('#shop-flters li');
-  
-      shopFilters.on('click', function(e) {
-        e.preventDefault();
-        shopFilters.each(function() {
-          $(this).removeClass('filter-active');
-        });
-        $(this).addClass('filter-active');
-        
-        var dataFilter = $(this).attr('data-filter');
-        var items = $('.shop-filter-item');
-        var count = 0;
+let shopFilterEle = $(".shop-filter-container>div");
+var dataFilter = "*";
+var priceFilter = 4000;
 
-        for(var i = 0; i < items.length; i++) {
-            if($(items[i]).hasClass(dataFilter)){
-                count++;
-            }
-        }
-        
-        for(var i = 0; i < items.length; i++) {
-            if(dataFilter == '*'){
-                $(items[i]).show();
-                $(items[i]).removeClass(function(index, className) {
-                    return className.split(' ').filter(c => c.includes('col-')).join(' ');
-                });
-                $(items[i]).addClass('col-lg-3 col-md-6 col-sm-12');
-            }
-            else if($(items[i]).hasClass(dataFilter)){
-                $(items[i]).show();
-                
-                switch(count){
-                    case 3:
-                        $(items[i]).removeClass('col-lg-3').addClass('col-lg-4');
-                        break;
-                    case 2:
-                        $(items[i]).removeClass('col-lg-3').addClass('col-lg-6');
-                        break;
-                    case 1:
-                        $(items[i]).removeClass('col-lg-3').addClass('col-lg-12');
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                $(items[i]).hide();
-                $(items[i]).removeClass(function(index, className) {
-                    return className.split(' ').filter(c => c.includes('col-')).join(' ');
-                });
-            }
-        }
+$(document).ready(function () {
+  if (shopFilterEle) {
+    let shopFilters = $("#shop-flters li");
+
+    shopFilters.on("click", function (e) {
+      e.preventDefault();
+      shopFilters.each(function () {
+        $(this).removeClass("filter-active");
+      });
+      $(this).addClass("filter-active");
+
+      dataFilter = $(this).attr("data-filter");
+      handleFilter();
+    });
+
+    //////////////////////////////////////////////////////////////////////////
+    // Input range change
+    //////////////////////////////////////////////////////////////////////////
+    const range = document.getElementById("price-range");
+    const rangeValue = document.querySelector(".range-value");
+
+    range.addEventListener("input", (event) => {
+      priceFilter = event.target.value * 50;
+
+      // Handle value on input range
+      const value =
+        "$" +
+        parseFloat(priceFilter).toLocaleString("en-US", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        });
+      rangeValue.textContent = value;
+      rangeValue.style.left = `calc(${event.target.value} * 1%)`;
+      rangeValue.style.setProperty("--value", `'${value}'`);
+
+      handleFilter();
+    });
+
+    //////////////////////////////////////////////////////////////////////////
+    // Input range change
+    //////////////////////////////////////////////////////////////////////////
+  }
+});
+
+function handleFilter() {
+  var items = $(".shop-filter-item");
+
+  for (var i = 0; i < items.length; i++) {
+    if (
+      dataFilter == "*" &&
+      items.eq(i).find(".item-price").attr("value") < priceFilter
+    ) {
+      $(items[i]).show();
+      $(items[i]).removeClass(function (index, className) {
+        return className
+          .split(" ")
+          .filter((c) => c.includes("w-"))
+          .join(" ");
+      });
+      $(items[i]).addClass("w-25");
+    } else if (
+      $(items[i]).hasClass(dataFilter) &&
+      items.eq(i).find(".item-price").attr("value") < priceFilter
+    ) {
+      $(items[i]).show();
+      $(items[i]).removeClass(function (index, className) {
+        return className
+          .split(" ")
+          .filter((c) => c.includes("w-"))
+          .join(" ");
+      });
+      $(items[i]).addClass("w-25");
+    } else {
+      $(items[i]).hide();
+      $(items[i]).removeClass(function (index, className) {
+        return className
+          .split(" ")
+          .filter((c) => c.includes("col-"))
+          .join(" ");
       });
     }
-  });
-  
+  }
+}
